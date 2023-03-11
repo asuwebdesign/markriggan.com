e2b8efd82c4554eb63a13184a294f172c72cc294
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2023-03-11 15:39:44 -0500

    Baseline Next JS framework

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+// import './globals.css'
+
+export const metadata = {
+  title: 'Mark Riggan',
+  description: 'Description...',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,85 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <header>
+      <h1>Mark Riggan</h1>
+    </header>
+    <main>
+      <section>
+        <h1>Reimagine possible.</h1>
+        <p>Connecting brands with people through handcrafted digital experiences.</p>
+
+        <div>
+          <h2>Human-centered design.</h2>
+          <p>Award-winning designs that are data-influenced and made for humans first.</p>
+        </div>
+        <div>
+          <h2>Top notch code.</h2>
+          <p>Architected and built with the latest technologies for scalability and longevity.</p>
+        </div>
+        <div>
+          <h2>Tailored to your business.</h2>
+          <p>Solutions that are tailored to the needs of your business. Nothing is cookie-cutter.</p>
+        </div>
+      </section>
+      <section>
+        <h1>Viable to valuable.</h1>
+        <p>Building identities and experiences to elevate and empower organizations of all shapes and sizes.</p>
+
+        {/* Featured projects */}
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+
+        {/* Latest Dribbble shots */}
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+
+        {/* Featured client logos */}
+      </section>
+      <section>
+        <h1>Simplifying complexity. Delightfully.</h1>
+        <p>Building identities and experiences to elevate and empower organizations of all shapes and sizes.</p>
+
+        <div>Branding Strategy &amp; Identity</div>
+        <div>Creative &amp; Experience Design</div>
+        <div>Research, Data &amp; Analysis</div>
+        <div>Websites &amp; Digital Platforms</div>
+        <div>Creative process Delightfully simple.</div>
+        <div>Personal manifesto Uncompromising design.</div>
+      </section>
+      <section>
+        <h1>Hashtag: winning.</h1>
+        <p>Building identities and experiences to elevate and empower organizations of all shapes and sizes.</p>
+        
+        {/* List of awards */}
+      </section>
+      <section>
+        <h1>Some light reading.</h1>
+        <p>Building identities and experiences to elevate and empower organizations of all shapes and sizes.</p>
+
+        {/* List of Medium articles */}
+      </section>
+    </main>
+    <footer>
+      <small>Copyright</small>
+    </footer>
+    </>
+  )
+}
